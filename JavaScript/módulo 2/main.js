dce832e638ef9f5c5380a5dc9c8b2004af503dc7
Sonia Tomé Godino <soniatg88@gmail.com>
2017-04-23 18:28:00 +0200

    Exercises JavaScript attachment

---
+++ JavaScript/módulo 2/main.js
@@ -0,0 +1,29 @@
+// Ejercicio 1
+
+var card = "4c";
+  if (card[0] === "3" && card[1] === "c") {
+    console.log ("Es un 3 de corazones");
+  } else {
+    if (card[0] === "1" && card[1] === "3" && card[2] === "c") {
+      console.log ( "es un rey de corazones") ;
+      }
+    else {
+      if (card[0] === "4" && card[1] === "c"){
+        console.log ( "es un cuatro de corazones");
+        }
+      }}
+
+//Ejercicio 2
+
+      for (var i = 2017; i < 2050; i = i + 4) {
+        console.log (i + " es un número bisiesto antes del 2050");
+      }
+
+//Ejercicio 3
+
+      var cards = 12
+      var sticks = 4
+      var redSticks = 2
+      var totalCards = card * sticks / redSticks
+
+      
