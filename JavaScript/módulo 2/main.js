22e0210c20c2c68f61bd0e17829fd87a9189c008
Sonia Tomé Godino <soniatg88@gmail.com>
2017-04-24 11:09:12 +0200

    exercise 1 y 2 corrected

--- JavaScript/módulo 2/main.js
+++ JavaScript/módulo 2/main.js
@@ -1,29 +1,42 @@
 // Ejercicio 1
 
+var targetNumber = "4";
+var targetSuit = "c";
+
+var target = {
+  number: "4",
+  suit: "c"
+};
+
 var card = "4c";
-  if (card[0] === "3" && card[1] === "c") {
-    console.log ("Es un 3 de corazones");
-  } else {
-    if (card[0] === "1" && card[1] === "3" && card[2] === "c") {
-      console.log ( "es un rey de corazones") ;
-      }
-    else {
-      if (card[0] === "4" && card[1] === "c"){
-        console.log ( "es un cuatro de corazones");
-        }
-      }}
+var suit = card[1];
+var number = card[0];
+
+if (number === target.number && suit === target.suit) {
+  console.log ("Es un 3 de corazones");
+} else if (card[0] === "1" && card[1] === "3" && card[2] === "c") {
+  console.log ( "es un rey de corazones") ;
+} else if (card[0] === target.number && card[1] === target.suit){
+  console.log ( "es un cuatro de corazones");
+}
 
 //Ejercicio 2
 
-      for (var i = 2017; i < 2050; i = i + 4) {
-        console.log (i + " es un número bisiesto antes del 2050");
+var leapYearInterval = 4;
+var lastYear = 2050;
+var currentYear = 2017;
+var YearSinceLastLeapYear = currentYear % leapYearInterval;
+var yearsUntilNextLeapYear = leapYearInterval - YearSinceLastLeapYear;
+var nextLeapYear = currentYear + yearsUntilNextLeapYear;
+
+
+for (var i = nextLeapYear; i < lastYear; i = i + leapYearInterval) {
+        console.log (i + " es un año bisiesto antes del 2050");
       }
 
 //Ejercicio 3
 
-      var cards = 12
+
       var sticks = 4
       var redSticks = 2
       var totalCards = card * sticks / redSticks
-
-      
